2680a7c1869c054ffc9655a9173775c913b4dfe0
Josh Sapers <josh@josh-Latitude-E6430.(none)>
2014-03-10 13:03:51 -0400

    adding lightcycle

--- hw6/LightCycle.py
+++ hw6/LightCycle.py
@@ -20,8 +20,11 @@ class LightCycleModel:
         self.trail1.append(Trail(self.cycle1.color,self.cycle1.height,0,self.cycle1.x,self.cycle1.y))
         self.trail2.append(Trail(self.cycle2.color,self.cycle2.height,0,self.cycle2.x,self.cycle2.y))
     def update(self):
-        self.cycle1.update()
+        """updates the cycles and the current paths for each lightcycle"""
+        self.cycle1.update()        
         self.cycle2.update()
+
+        #enlongating the path for cycle 1        
         if self.cycle1.vx>0:
            self.trail1[-1].width+=self.cycle1.vx
         if self.cycle1.vx<0:
@@ -32,7 +35,8 @@ class LightCycleModel:
         if self.cycle1.vy<0:
             self.trail1[-1].height=self.trail1[-1].height-self.cycle1.vy
             self.trail1[-1].y = self.cycle1.y+self.cycle1.height
-       
+
+        #enlongating the path for cycle 2      
         if self.cycle2.vx>0:
            self.trail2[-1].width+=self.cycle2.vx
         if self.cycle2.vx<0:
@@ -43,10 +47,11 @@ class LightCycleModel:
         if self.cycle2.vy<0:
             self.trail2[-1].height=self.trail2[-1].height-self.cycle2.vy
             self.trail2[-1].y = self.cycle2.y+self.cycle2.height
-        print "\n\n\n\n"
+        
         for path1 in self.trail1[:-1]:
-            print path1.x, path1.y
+            #Checking for collisions with paths
             if pygame.Rect(self.cycle1.x,self.cycle1.y,self.cycle1.width,self.cycle1.height).colliderect(pygame.Rect(path1.x,path1.y,path1.width,path1.height)):
+                #ends game if it collides                
                 return False
         for path1 in self.trail1[:]:
             if pygame.Rect(self.cycle2.x,self.cycle2.y,self.cycle2.width,self.cycle2.height).colliderect(pygame.Rect(path1.x,path1.y,path1.width,path1.height)):               
@@ -57,6 +62,7 @@ class LightCycleModel:
         for path2 in self.trail2[:-1]:
             if pygame.Rect(self.cycle2.x,self.cycle2.y,self.cycle2.width,self.cycle2.height).colliderect(pygame.Rect(path2.x,path2.y,path2.width,path2.height)):
                 return False
+        #ends game if the cycle hit the edge of screen
         if not(self.windowRect.contains(pygame.Rect(self.cycle1.x,self.cycle1.y,self.cycle1.width,self.cycle1.height))):
             return False
         if not(self.windowRect.contains(pygame.Rect(self.cycle2.x,self.cycle2.y,self.cycle2.width,self.cycle2.height))):
@@ -74,6 +80,7 @@ class Cycle:
         self.vy = 0.0
         
     def update(self):
+        """Changes the location based off of the two velocities"""
         self.x += self.vx
         self.y += self.vy
         
@@ -86,12 +93,13 @@ class Trail:
         self.x=x
         self.y=y        
 class PyGameWindowView:
-    """ A view of brick breaker rendered in a Pygame window """
+    """ A view of LightCycles rendered in a Pygame window """
     def __init__(self,model,screen,size):
         self.model = model
         self.model.windowRect=pygame.Rect(0,0,size[0],size[1])
         self.screen = screen
     def draw(self):
+        """draws the two cycles and all of the paths they made"""
         self.screen.fill(pygame.Color(0,0,0))
         pygame.draw.rect(self.screen, pygame.Color(self.model.cycle1.color[0],self.model.cycle1.color[1],self.model.cycle1.color[2]),pygame.Rect(self.model.cycle1.x,self.model.cycle1.y,self.model.cycle1.width,self.model.cycle1.height))
         pygame.draw.rect(self.screen, pygame.Color(self.model.cycle2.color[0],self.model.cycle2.color[1],self.model.cycle2.color[2]),pygame.Rect(self.model.cycle2.x,self.model.cycle2.y,self.model.cycle2.width,self.model.cycle2.height))
@@ -106,6 +114,8 @@ class Controller:
         self.model = model
     
     def handle_keyboard_event(self,event):
+        """checks for wasd or arrow key inputs and changes the direction accordingly. Also adds
+            a new path when the cycle changes direction"""
         if event.type != KEYDOWN:
             return
         if event.key == pygame.K_LEFT and self.model.cycle1.vx == 0:
